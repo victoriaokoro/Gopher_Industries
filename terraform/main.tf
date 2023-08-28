f019fee54b8161874db2ae95e378230fa603a18c
Lachlan Vass <lachlan.vass@okendo.io>
2023-08-28 11:07:34 +1000

    format

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,26 +1,26 @@
 terraform {
-    required_providers {
-        google = {
-            source = "hashicorp/google"
-            version = "4.51.0"
-        }
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
     }
-    
-    backend "gcs" {
-        bucket = "foodremedy-api-tfstate"
-        prefix = "env/dev"
-    }
+  }
+
+  backend "gcs" {
+    bucket = "foodremedy-api-tfstate"
+    prefix = "env/dev"
+  }
 }
 
 provider "google" {
-    credentials = file("credentials.json")
+  credentials = file("credentials.json")
 
-    project = "sit-23t2-food-remedy-7f9b4c0"
-    region  = "australia-southeast2"
+  project = "sit-23t2-food-remedy-7f9b4c0"
+  region  = "australia-southeast2"
 }
 
 resource "google_storage_bucket" "example" {
-    name = "foodremedy-api-example"
-    location = "AUSTRALIA-SOUTHEAST2"
-    
+  name     = "foodremedy-api-example"
+  location = "AUSTRALIA-SOUTHEAST2"
+
 }
