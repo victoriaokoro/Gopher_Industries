913f357e64390ee9af534ab6d4f53d6d97f01a8a
Lachlan Vass <lachlan.vass@okendo.io>
2023-09-10 16:39:57 +1000

    add registry config and more variables

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,6 +15,20 @@ terraform {
 provider "google" {
   credentials = file("credentials.json")
 
-  project = "sit-23t2-food-remedy-7f9b4c0"
-  region  = "australia-southeast2"
+  project = var.project
+  region  = var.region
+}
+
+resource "google_artifact_registry_repository" "foodremedy_backend" {
+  repository_id = "${var.env}-foodremedy-backend"
+  format = "DOCKER"
+  location = var.region
+  project = var.project
+}
+
+resource "google_artifact_registry_repository" "foodremedy_database" {
+  repository_id = "${var.env}-foodremedy-database"
+  format = "DOCKER"
+  location = var.region
+  project = var.project
 }
