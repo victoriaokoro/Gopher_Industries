b553425ed3bbc69f8c690fd081a19978715ae91f
Lachlan Vass <lachlan.vass@okendo.io>
2023-09-14 16:16:03 +1000

    formatting

--- terraform/cluster.tf
+++ terraform/cluster.tf
@@ -4,24 +4,24 @@ resource "google_service_account" "foodremedy_clusterServiceAccount" {
 }
 
 resource "google_container_cluster" "foodremedy_cluster" {
-  name               = "${var.env}-foodremedy-cluster"
-  location           = var.region
-  initial_node_count = 1
+  name                     = "${var.env}-foodremedy-cluster"
+  location                 = var.region
+  initial_node_count       = 1
   remove_default_node_pool = true
 }
 
 resource "google_container_node_pool" "foodremedy_clusterNodePool" {
-    name = "${var.env}-foodremedy-cluster-node-pool"
-    location = var.region
-    cluster = google_container_cluster.foodremedy_cluster.name
-    node_count = 1
+  name       = "${var.env}-foodremedy-cluster-node-pool"
+  location   = var.region
+  cluster    = google_container_cluster.foodremedy_cluster.name
+  node_count = 1
 
-    node_config {
+  node_config {
     preemptible  = true
     machine_type = "e2-medium"
 
     service_account = google_service_account.foodremedy_clusterServiceAccount.email
-    oauth_scopes    = [
+    oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
     ]
   }
