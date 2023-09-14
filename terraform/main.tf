bf5d3c99427263ba4a1189f051461389d435773f
Lachlan Vass <lachlan.vass@okendo.io>
2023-09-14 15:26:25 +1000

    add repo for frontend

--- terraform/main.tf
+++ terraform/main.tf
@@ -32,3 +32,11 @@ resource "google_artifact_registry_repository" "foodremedy_database" {
   location = var.region
   project = var.project
 }
+
+
+resource "google_artifact_registry_repository" "foodremedy_frontend" {
+  repository_id = "${var.env}-foodremedy-frontend"
+  format = "DOCKER"
+  location = var.region
+  project = var.project
+}
