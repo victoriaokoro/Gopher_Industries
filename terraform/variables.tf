fb8aa352d6da35b9fce9424d6d5d83e88bd05827
Lachlan Vass <lachlan.vass@okendo.io>
2023-09-14 15:56:21 +1000

    add sensitive variable to set mysql database

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,3 +9,8 @@ variable "region" {
 variable "project" {
   default = "sit-23t2-food-remedy-7f9b4c0"
 }
+
+variable "foodremedy_database_root_password" {
+  sensitive = true
+  description = "Must assign value at deployment time. Do not hardcode in Terraform."
+}
